e46d468fb6fbf14633ed270690627b2dfd9b65af
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-05 21:12:14 +0000

    We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
--- A  script that creates a table users
-CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO INCREMENT AND PRIMARY KEY,
-	email VARCHAR(255) NOT NULL AND UNIQUE,
-	name VARCHAR(255)
+-- a script creates a table with the unique users
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 );
