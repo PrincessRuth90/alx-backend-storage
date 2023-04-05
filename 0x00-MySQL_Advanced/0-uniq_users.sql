45a9060a1b2f2e8c3deb479968c67678741b0187
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-05 20:05:58 +0000

    We are all unique!

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 # A  script that creates a table users
 CREATE TABLE users (
-	id int, never null, auto increment and primary key,
-	email varchar(255) never null and unique,
-	name varchar(255)
+	id INT NOT NULL AUTO INCREMENT AND PRIMARY KEY,
+	email VARCHAR(255) NOT NULL AND UNIQUE,
+	name VARCHAR(255)
 );
