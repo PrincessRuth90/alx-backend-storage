a812fe998b8531d6281196e9ce8a2673af6d4b79
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-05 21:26:30 +0000

    In and not out

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,6 @@
 -- a script creates a table with the unique users
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT AND PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
-# A  script that creates a table users
+-- A  script that creates a table users
 CREATE TABLE users (
-	id int, never null, auto increment and primary key,
-	email varchar(255) never null and unique,
-	name varchar(255)
-	country char(2) never null (= default will be the first element of the enumeration, here US)
+    id INT NOT NULL AUTO INCREMENT AND PRIMARY KEY,
+    email VARCHAR(255) NOT NULL AND UNIQUE,
+    name VARCHAR(255)
+    country CHAR(2) NOT NULL (= default will be the first element of the enumeration, here US)
 );
