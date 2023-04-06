8c716b148f8158ae889d785c9cea39cb4d827b06
PrincessRuth90 <marcusruth86@gmail.com>
2023-04-06 22:35:12 +0000

    Average weighted score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,15 +1,33 @@
 -- a script that creates a stored procedure ComputeAverageWeightedScoreForUser
 -- that computes and store the average weighted score for a student.
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-	IN user_id int
-)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (user_id INT)
 BEGIN
-   UPDATE users
-   SET overall_score = (SELECT AVG(score)
-    FROM corrections
-	WHERE corrections.user_id=user_id
-	GROUP BY corrections.user_id )
-    WHERE id=user_id;
-END //
-DELIMITER;
+    DECLARE total_weighted_score INT DEFAULT 0;
+    DECLARE total_weight INT DEFAULT 0;
+
+    SELECT SUM(corrections.score * projects.weight)
+        INTO total_weighted_score
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    SELECT SUM(projects.weight)
+        INTO total_weight
+        FROM corrections
+            INNER JOIN projects
+                ON corrections.project_id = projects.id
+        WHERE corrections.user_id = user_id;
+
+    IF total_weight = 0 THEN
+        UPDATE users
+            SET users.average_score = 0
+            WHERE users.id = user_id;
+    ELSE
+        UPDATE users
+            SET users.average_score = total_weighted_score / total_weight
+            WHERE users.id = user_id;
+    END IF;
+END $$
+DELIMITER ;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,22 @@
 -- Creates a stored procedure, ComputeOverallScoreForUser that computes stores
 -- stores the overall score for a student
-
-DELIMITER //
-CREATE PROCEDURE ComputeOverallScoreForUser(
-	IN user_id int
-)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-   UPDATE users
-   SET overall_score = (SELECT AVG(score)
-    FROM corrections
-	WHERE corrections.user_id=user_id
-	GROUP BY corrections.user_id )
-    WHERE id=user_id;
-END //
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
+
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
